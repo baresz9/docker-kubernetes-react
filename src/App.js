5d9d3a2a1d70ab5532ee041ac9eaacd4e8c200c6
baresz9 <baresz@Baresz-pc.localdomain>
2022-06-15 15:03:36 +0200

    first deployment testing

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          testing2
+          Nezd Petra mukszik az appom AWS-en :)
         </p>
         <a
           className="App-link"
